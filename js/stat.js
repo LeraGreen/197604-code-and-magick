9592ba679d955202fc40db705ccf8cab057bee40
Lera Green <zelnayavaleriya@gmail.com>
2018-02-04 23:52:58 +0300

    делает много дичи

--- js/stat.js
+++ js/stat.js
@@ -1,40 +1,121 @@
 'use strict';
 
-const Statistics = function (ctx, names, times) {
+const renderNumbers = () => {
+  const max = 1;
+  const min = 0;
+  return ((Math.random() * (max - min)) + min);
+};
+
+const renderText = function (ctx, text, fontParams) {
+  ctx.fillStyle = fontParams.color;
+  ctx.font = fontParams.font;
+  ctx.fillText(text, fontParams.x, fontParams.y);
+};
+
+// const Color = function () {
+//   this.userColumnColor = {
+//     red: 255,
+//     green: 0,
+//     blue: 0,
+//     alfa: 1
+//   };
+//   this.otherColumnsColor = {
+//     red: 0,
+//     green: 191,
+//     blue: 255,
+//     alfa: null
+//   };
+//
+//   this.renderAlfa = function () {
+//     return +(renderNumbers() + 0.2).toFixed(1);
+//   };
+//
+//   this.getRgbaString = function () {
+//
+//   };
+// };
+
+// TODO(@greenlera) генерацию числа вынести
+// TODO(@greenlera) переименовать объект в renderer
+// TODO(@greenlera) вынести объект color
+// TODO(@greenlera) вынести простую логику из методов
+// TODO(@greenlera) отрисовку текстов
+// TODO(@greenlera) не вызыват снаружи методы
+// TODO(@greenlera) перепридумать публичный интерфейс
+// TODO(@greenlera) добавить менеджера
+
+
+// var ctx = canvas.getContext('2d');
+// var players = [4000, 4200, 1400, 3500];
+//
+// var height = 150;
+// var width = 20;
+//
+// var offset = 20;
+//
+// var max = Math.max.apply(null, players);
+//
+// players.forEach(function(player, index) {
+//   var columnHeight = (player * height) / max;
+//
+//   ctx.fillRect((width + offset) * index, height - columnHeight, width, columnHeight);
+// });
+
+const StatisticsRenderer = function (ctx, names, times) {
   this.names = names;
   this.ctx = ctx;
-  this.font = `16px PT Mono`;
-  this.fontColor = `black`;
-  this.cloudX = 100;
-  this.cloudY = 10;
-  this.cloudWidth = 420;
-  this.cloudHeight = 270;
-  this.columnWidth = 40;
-  this.columnMaxHeight = 150;
-  this.columnOffset = 50;
-  this.padding = 5;
-  this.userColumnColor = `rgba(255, 0, 0, 1)`;
-  this.otherColumnsColor = `rgb(0,191,255)`;
+  this.fontOptions = {
+    color: `black`,
+    font: `16px PT Mono`,
+    x: 0,
+    y: 0
+  };
+  this.cloudOptions = {
+    cloudX: 100,
+    cloudY: 10,
+    cloudWidth: 420,
+    cloudHeight: 270,
+    cloudOffset: 10,
+    mainCloudColor: `white`,
+    shadowCloudColor: `rgba(0, 0, 0, 0.7)`
+  };
+  this.columnOptions = {
+    columnWidth: 40,
+    columnMaxHeight: 150,
+    columnOffset: 50,
+    padding: 5
+  };
   this.times = times.map((time) => {
     return Math.round(time);
   });
 
+  this.render = function () {
+    this.renderCloud(
+        this.cloudOptions.cloudY + this.cloudOptions.cloudOffset, this.cloudOptions.cloudY + this.cloudOptions.cloudOffset, this.cloudOptions.shadowCloudColor);
+    this.renderCloud(
+        this.cloudOptions.cloudX,
+        this.cloudOptions.cloudY,
+        this.cloudOptions.mainCloudColor);
+    renderText(
+        this.ctx,
+        `Ура! Вы победили!`,
+        Object.assign({},
+            this.fontOptions, {
+              x: 220,
+              y: 40
+            }
+        ));
+    this.renderColumns();
+  };
+
 
   this.renderCloud = function (x, y, color) {
     this.ctx.fillStyle = color;
-    this.ctx.fillRect(x, y, this.cloudWidth, this.cloudHeight);
-  };
-
-  this.renderText = function (text, x, y) {
-    this.ctx.fillStyle = this.fontColor;
-    this.ctx.font = this.font;
-    this.ctx.fillText(text, x, y);
+    this.ctx.fillRect(x, y, this.cloudOptions.cloudWidth, this.cloudOptions.cloudHeight);
   };
 
   this.renderNumbers = function () {
-    const max = 1;
-    const min = 0;
-    return +((Math.random() * (max - min)) + min + 0.2).toFixed(1);
+    return +(renderNumbers() + 0.2).toFixed(1);
   };
 
   this.renderColor = function () {
@@ -46,22 +127,27 @@ const Statistics = function (ctx, names, times) {
   this.renderColumns = function () {
     const max = Math.max.apply(null, this.times);
     this.names.forEach(function (name, index) {
-      const x = this.cloudX + this.columnOffset + ((this.columnWidth + this.columnOffset) * index);
-      const y = Math.round((-this.columnMaxHeight * this.times[index]) / max);
-      this.renderText(name, x, this.cloudHeight + this.padding);
+      const x = this.cloudOptions.cloudX + this.columnOptions.columnOffset + ((this.columnOptions.columnWidth + this.columnOptions.columnOffset) * index);
+      const y = Math.round((-this.columnOptions.columnMaxHeight * this.times[index]) / max);
+
+      renderText(this.ctx, name,
+          Object.assign({}, this.fontOptions, {
+            x,
+            y: this.cloudOptions.cloudHeight + this.columnOptions.padding
+          }));
       this.ctx.fillStyle = name === `Вы` ? this.userColumnColor : this.renderColor();
       this.ctx.fillRect(x, 250, 40, y);
-      this.renderText(this.times[index], x, this.cloudHeight - (-y) - this.padding * 5);
+      renderText(this.ctx, this.times[index],
+          Object.assign({}, this.fontOptions, {
+            x,
+            y: this.cloudOptions.cloudHeight - (-y) - this.columnOptions.padding * 5
+          }));
     }, this);
   };
 };
 
 
 window.renderStatistics = function (ctx, names, times) {
-  const statistics = new Statistics(ctx, names, times);
-
-  statistics.renderCloud(110, 20, `rgba(0, 0, 0, 0.7)`);
-  statistics.renderCloud(100, 10, `white`);
-  statistics.renderText(`Ура! Вы победили!`, 220, 40);
-  statistics.renderColumns();
+  const renderer = new StatisticsRenderer(ctx, names, times);
+  renderer.render();
 };
